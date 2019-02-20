37c70ddd64c7093f20151208b807b45b3611ff42
yaap <gracieux.guillaume@gmail.com>
2019-02-20 11:21:37 -0500

    correct the unhex-passwords.py in case password contain a \9

--- unhex-passwords.py
+++ unhex-passwords.py
@@ -8,11 +8,17 @@ def main():
     regex = re.compile('\$HEX\[([a-fA-F0-9]*)\]')
     for line in sys.stdin:
         while re.search(regex, line):
-            decoded = bytes.fromhex(re.search(regex, line).group(1)).decode('utf-8')
-            line = regex.sub(decoded, line.strip())
+            try:
+                decoded = bytes.fromhex(re.search(regex, line).group(1)).decode('iso-8859-1')
+                # patch to avoid issue with '\9' in decoded
+                decoded = decoded.replace('\\', '\\\\')
+                line = regex.sub(decoded, line.strip())
+            except Exception as e:
+                sys.stderr.write('Error with line = {}'.format(line))
+                sys.stderr.write('decoded = {}'.format(decoded))
+                raise e
         print(line.strip())
 
 
 if __name__ == '__main__':
     main()
-
