cfe6f68536a99c170fc7bed1c91167f0dc52531b
yaap <gracieux.guillaume@gmail.com>
2019-02-22 10:34:17 -0500

    added examples and modular function for unhex-passwords.py

--- unhex-passwords.py
+++ unhex-passwords.py
@@ -4,21 +4,24 @@ import re
 import sys
 
 
-def main():
+def unhex_line(line):
     regex = re.compile('\$HEX\[([a-fA-F0-9]*)\]')
-    for line in sys.stdin:
-        while re.search(regex, line):
-            try:
-                decoded = bytes.fromhex(re.search(regex, line).group(1)).decode('iso-8859-1')
-                # patch to avoid issue with '\9' in decoded
-                decoded = decoded.replace('\\', '\\\\')
-                line = regex.sub(decoded, line.strip())
-            except Exception as e:
-                sys.stderr.write('Error with line = {}'.format(line))
-                sys.stderr.write('decoded = {}'.format(decoded))
-                raise e
-        print(line.strip())
+    while re.search(regex, line):
+        try:
+            decoded = bytes.fromhex(re.search(regex, line).group(1)).decode('iso-8859-1')
+            # patch to avoid issue with '\9' in decoded
+            decoded = decoded.replace('\\', '\\\\')
+            line = regex.sub(decoded, line.strip())
+        except Exception as e:
+            sys.stderr.write('Error with line = {}'.format(line))
+            sys.stderr.write('decoded = {}'.format(decoded))
+            raise e
+    return(line.strip())
 
+
+def main():
+    for line in sys.stdin:
+        print(unhex_line(line))
 
 if __name__ == '__main__':
     main()
